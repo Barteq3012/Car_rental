21068d998c3c820b6ac6b6313e546a45c3db59e2
Barteq3012 <bartek.cz3012@gmail.com>
2022-01-03 18:45:45 +0100

    First step to UI

---
+++ CarRental.WebApp/Controllers/CompanyController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using CarRental.WebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+
+namespace CarRental.WebApp.Controllers
+{
+    public class CompanyController : Controller
+    {
+        public IConfiguration Configuration;
+        public CompanyController(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public ContentResult GetHostUrl()
+        {
+            var result = Configuration["RestApiUrl:HostUrl"];
+            return Content(result);
+        }
+
+        private string CN()
+        {
+            return ControllerContext.RouteData.Values["controller"].ToString();
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            string _restpath = GetHostUrl().Content + CN();
+            List<CompanyVM> companyList = new List<CompanyVM>();
+
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync(_restpath))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    companyList = JsonConvert.DeserializeObject<List<CompanyVM>>(apiResponse);
+                }
+                
+            }
+            return View(companyList);
+        }
+    }
+}
---
+++ CarRental.WebApp/Models/CompanyVM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarRental.WebApp.Models
+{
+    public class CompanyVM
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Country { get; set; }
+    }
+}
