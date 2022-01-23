110d53383e80b0fbc8bed2087c6df79baf4b04ec
Barteq3012 <bartek.cz3012@gmail.com>
2022-01-23 01:24:23 +0100

    Dodanie pierwszego interfejsu - dla Company

--- CarRental.WebApp/Controllers/CompanyController.cs
+++ CarRental.WebApp/Controllers/CompanyController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 using CarRental.WebApp.Models;
 using Microsoft.AspNetCore.Mvc;
@@ -45,5 +46,114 @@ namespace CarRental.WebApp.Controllers
             }
             return View(companyList);
         }
+
+        public IActionResult Create()
+        {
+             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CompanyVM company)
+        {
+            string _restpath = GetHostUrl().Content + CN();
+
+            try {
+
+                using (var httpClient = new HttpClient())
+                {
+                    string jsonString = System.Text.Json.JsonSerializer.Serialize(company);
+                    var content = new StringContent(jsonString, Encoding.UTF8, "application/json");
+                    await httpClient.PostAsync(_restpath, content);
+                    return RedirectToAction(nameof(Index));
+                }
+            } 
+            catch(Exception ex)
+            {
+                return View(ex);
+            }
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            string _restpath = GetHostUrl().Content + CN();
+            CompanyVM company = new CompanyVM();
+
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync($"{_restpath}/{id}"))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    company = JsonConvert.DeserializeObject<CompanyVM>(apiResponse);
+                }
+                
+            }
+            return View(company);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(CompanyVM company)
+        {
+            string _restpath = GetHostUrl().Content + CN();
+
+            try {
+
+                using (var httpClient = new HttpClient())
+                {
+                    string jsonString = System.Text.Json.JsonSerializer.Serialize(company);
+                    var content = new StringContent(jsonString, Encoding.UTF8, "application/json");
+
+                    using (var response = await httpClient.PutAsync($"{_restpath}/{company.Id}", content ))
+                    {
+                        string apiResponse = await response.Content.ReadAsStringAsync();
+                    }
+                
+                }
+            } 
+            catch(Exception ex)
+            {
+                return View(ex); //tu trzeba by obsłużyć ten wyjątek
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            string _restpath = GetHostUrl().Content + CN();
+
+            CompanyVM company = new CompanyVM();
+
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync($"{_restpath}/{id}"))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    company = JsonConvert.DeserializeObject<CompanyVM>(apiResponse);
+                }
+                
+            }
+            return View(company);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(CompanyVM company)
+        {
+            string _restpath = GetHostUrl().Content + CN();
+
+            try {
+
+                using (var httpClient = new HttpClient())
+                {
+                    var response = await httpClient.DeleteAsync($"{_restpath}/{company.Id}");       
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+            } 
+            catch(Exception ex)
+            {
+                return View(ex);
+            } 
+        }
+
     }
 }
--- CarRental.WebApp/Models/CompanyVM.cs
+++ CarRental.WebApp/Models/CompanyVM.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,8 +9,11 @@ namespace CarRental.WebApp.Models
     public class CompanyVM
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Enter Name!")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "Enter Address!")]
         public string Address { get; set; }
+        [Required(ErrorMessage = "Enter Country!")]
         public string Country { get; set; }
     }
 }
